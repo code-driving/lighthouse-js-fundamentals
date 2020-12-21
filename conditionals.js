499edc38609ec2653858a1f2d78edc3d218b3249
code-driving <tittan7@gmail.com>
2020-12-21 05:50:58 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -75,14 +75,71 @@
 
 // console.log("Now you are ready to go outside!");
 
-const temperature = -5;
+// const temperature = -5;
 
-if (temperature < 0) {
-  console.log("Make sure you pick out a scarf!");
-} else if (temperature < 15) {
-  console.log("Short sleeves won't cut it!");
-} else {
-  console.log("Short sleeves are fine");
-}
+// if (temperature < 0) {
+//   console.log("Make sure you pick out a scarf!");
+// } else if (temperature < 15) {
+//   console.log("Short sleeves won't cut it!");
+// } else {
+//   console.log("Short sleeves are fine");
+// }
 
-console.log("Now you are ready to go outside!");
+// console.log("Now you are ready to go outside!");
+
+// const isCitizen = true;
+// const age = 26;
+
+// if (isCitizen && age > 18) {
+//   console.log("You are eligible to vote.");
+// }
+
+// const isCitizen = false;
+// const age = 26;
+
+// if (isCitizen && age > 18) {
+//   console.log("You are eligible to vote.");
+// } else {
+//   console.log("You are not eligible to vote. Go home.");
+// }
+
+// const isCitizen = true;
+// const age = 16;
+
+// if (isCitizen && age > 18) {
+//   console.log("You are eligible to vote.");
+// } else {
+//   console.log("You are not eligible to vote. Go home.");
+// }
+
+// const temperature = 45;
+
+// if (temperature < -40 || temperature > 40) {
+//   console.log(
+//     "Maybe going outside isn't such a great idea... Stay home and have some coffee."
+//   );
+// }
+
+// const temperature = -50;
+
+// if (temperature < -40 || temperature > 40) {
+//   console.log(
+//     "Maybe going outside isn't such a great idea... Stay home and have some coffee."
+//   );
+// }
+
+// const raining = true;
+
+// if (!raining) {
+//   console.log("Leave your umbrella at home!");
+// } else {
+//   console.log("Grab you umbrella with you!");
+// }
+
+// const raining = false;
+
+// if (!raining) {
+//   console.log("Leave your umbrella at home!");
+// } else {
+//   console.log("Grab you umbrella with you!");
+// }
