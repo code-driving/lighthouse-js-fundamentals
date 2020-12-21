18e66e5a743b0b2c95aa252b6b5ff725975940b2
code-driving <tittan7@gmail.com>
2020-12-21 05:53:03 +0000

    Tinkering with code

---
+++ tinkering.js
@@ -0,0 +1,64 @@
+// function multiply(x, y) {
+//   return x > 5 ? x / y : x * 5;
+// }
+// const result = multiply(25, 5);
+// console.log(result);
+
+// const result1 = multiply(4, 5);
+// console.log(result1);
+
+// function add(a, b) {
+//   return a + b;
+// }
+// const additionResult = add(4, 0);
+// console.log(additionResult);
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+// const result = multiply(2, 4);
+// console.log(result);
+
+// function multiply(a, b) {
+//   return a * b;
+// }
+
+// const result = multiply(2, -4);
+// console.log(result);
+
+// function divide(a, b) {
+//   return a / b;
+// }
+// const divisionResult = divide(0, 1);
+// console.log(divisionResult);
+
+// function addWhenNotFalse(a, b) {
+//   if (a) {
+//     return a + b;
+//   } else {
+//     console.log("Falsy a");
+//   }
+// }
+// const result = addWhenNotFalse(1, 2);
+// console.log(result);
+// const negativeResult = addWhenNotFalse(false, 4);
+// console.log(negativeResult);
+
+// function substract(a, b) {
+//   return a - b;
+// }
+// const result = substract(80, 5);
+// console.log(result);
+
+// function playWithOperators(a, b, c = 5) {
+//   return a + b * c;
+// }
+// const result = playWithOperators(1, 2);
+// console.log(result);
+
+// function playWithOperators(a, b, c = 5) {
+//   return a + b * c;
+// }
+// const result = playWithOperators(1, 2, 10);
+// console.log(result);
