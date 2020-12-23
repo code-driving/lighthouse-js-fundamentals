e7afb5691cf0022c7151ca353678918e66090c85
code-driving <tittan7@gmail.com>
2020-12-23 09:38:24 +0000

    Some of my solutions for Udacity Lesson 4

---
+++ myUdacity.js
@@ -0,0 +1,80 @@
+//JuliaJames Udacity
+// let x = 1;
+
+// while (x <= 20) {
+//   x % 3 === 0
+//     ? x % 5 === 0
+//       ? console.log("JuliaJames")
+//       : console.log("Julia")
+//     : x % 5 === 0
+//     ? console.log("James")
+//     : console.log(x);
+
+//   x += 1;
+// }
+
+// 99 bottles of juice
+// var num = 99;
+
+// while (num > 0) {
+//   if (num > 2) {
+//     console.log(
+//       `${num} bottles of juice on the wall! ${num} bottles of juice! Take one down, pass it around... ${
+//         num - 1
+//       } bottles of juice on the wall!`
+//     );
+//   } else if (num === 2) {
+//     console.log(
+//       `${num} bottles of juice on the wall! ${num} bottles of juice! Take one down, pass it around... ${
+//         num - 1
+//       } bottle of juice on the wall!`
+//     );
+//   } else if (num === 1) {
+//     console.log(
+//       `${num} bottle of juice on the wall! ${num} bottle of juice! Take one down, pass it around... ${
+//         num - 1
+//       } bottles of juice on the wall!`
+//     );
+//   }
+
+//   num -= 1;
+// }
+
+//Countdown. Liftoff.
+
+// let x = 60;
+
+// while (x >= 0) {
+//   if (x === 50) {
+//     console.log("Orbiter transfers from ground to internal power");
+//   } else if (x === 31) {
+//     console.log("Ground launch sequencer is go for auto sequence start");
+//   } else if (x === 16) {
+//     console.log("Activate launch pad sound suppression system");
+//   } else if (x === 10) {
+//     console.log("Activate main engine hydrogen burnoff system");
+//   } else if (x === 6) {
+//     console.log("Main engine start");
+//   } else if (x === 0) {
+//     console.log("Solid rocket booster ignition and liftoff!");
+//   } else {
+//     console.log(`T - ${x} seconds`);
+//   }
+
+//   x -= 1;
+// }
+
+//Quiz Factorials
+// let solution = 1;
+
+// for (let n = 12; n >= 1; n--) {
+//     solution *= n;
+// }
+// console.log(solution);
+
+//Find my seat
+// for (let row = 0; row < 26; row++) {
+//   for (let seat = 0; seat < 100; seat++) {
+//       console.log(row + "-" + seat);
+//   }
+// }
