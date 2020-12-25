adab405feac5c5f9b6a937b6e304800176fcfde5
code-driving <tittan7@gmail.com>
2020-12-25 02:49:40 +0000

    Added two examples with return

--- hello.js
+++ hello.js
@@ -4,11 +4,33 @@
 
 // sayHello();
 
-const sayHello = function (name) {
-  console.log("Hello, " + name);
-};
+// const sayHello = function (name) {
+//   console.log("Hello, " + name);
+// };
 
-sayHello("Jake");
-sayHello("Caliban");
-sayHello("Miranda");
-sayHello("Ferdinand");
+// sayHello("Jake");
+// sayHello("Caliban");
+// sayHello("Miranda");
+// sayHello("Ferdinand");
+
+//console.log
+// const sayHelloToConsole = function (name) {
+//   console.log("Hello, " + name);
+// };
+
+// sayHelloToConsole("John");
+
+//return
+// const returnSayHello = function (name) {
+//   return "Hello, " + name;
+// };
+
+// const greeting = returnSayHello("John");
+
+//return and console.log
+
+// const returnSayHello = function (name) {
+//   return "Hello, " + name;
+// };
+// const greeting = returnSayHello("John");
+// console.log(greeting);
