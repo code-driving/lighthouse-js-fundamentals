e0a1ad39a60bd8f39c3e00a081aa27ea41e3ff90
code-driving <tittan7@gmail.com>
2020-12-25 02:40:47 +0000

    Function greeting a person

---
+++ hello.js
@@ -0,0 +1,14 @@
+// const sayHello = function () {
+//   console.log("Hello, world");
+// };
+
+// sayHello();
+
+const sayHello = function (name) {
+  console.log("Hello, " + name);
+};
+
+sayHello("Jake");
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
