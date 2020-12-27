cb8c311d10468c5d847c801111e4b5384b1bdc87
code-driving <tittan7@gmail.com>
2020-12-27 23:59:54 +0000

    Looping - Range assignment

---
+++ looping-range.js
@@ -0,0 +1,35 @@
+// const range = function (start, end, step) {
+//   let evenNumbersInRange = [];
+
+//   for (let i = start; i <= end; i += step) {
+//     if (start !== undefined && end !== undefined && step !== undefined) {
+//       if (start < end && step > 0) {
+//         if (i % 2 === 0) {
+//           evenNumbersInRange.push(i);
+//         }
+//       }
+//     }
+//   }
+//   return evenNumbersInRange;
+// };
+
+// console.log(range(0, 10, 2));
+// console.log(range(10, 30, 5));
+// console.log(range(-5, 2, 3));
+
+const range = function (start, end, step) {
+  let numbers = [];
+
+  for (let i = start; i <= end; i += step) {
+    if (start !== undefined && end !== undefined && step !== undefined) {
+      if (start < end && step > 0) {
+        numbers.push(i);
+      }
+    } else return [];
+  }
+  return numbers;
+};
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
+console.log(range(31, 30, 5));
